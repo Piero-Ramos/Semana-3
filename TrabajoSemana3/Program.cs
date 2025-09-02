5b66033ce2ff74d2ad9f8dcd4a50dbcf28bb06d2
rodrigo418q <rrm41810@gmail.com>
2025-09-02 09:35:20 -0500

    actualizacion de codigo
    3 pesos de la nota

--- TrabajoSemana3/Program.cs
+++ TrabajoSemana3/Program.cs
@@ -10,12 +10,10 @@ namespace TrabajoSemana3
     {
         static void Main(string[] args)
         {
-            string nota1, nota2, nota3;
+            string nota1, nota2, nota3, p1, p2, p3;
             double peso1, peso2, peso3, n1, n2, n3;
             double notaponderada1, notaponderada2, notaponderada3, promediofinal;
-            peso1 = 20;
-            peso2 = 30;
-            peso3 = 50;
+            
 
             Console.WriteLine("Ingrese las notas de cada examen: ");
             nota1 = Console.ReadLine();
@@ -26,9 +24,19 @@ namespace TrabajoSemana3
             n2 = double.Parse(nota2);
             n3 = double.Parse(nota3);
 
+            Console.WriteLine("Ingrese el peso de cada examen: ");
+            p1 = Console.ReadLine();
+            p2 = Console.ReadLine();
+            p3 = Console.ReadLine();
+            peso1 = double.Parse(p1);
+            peso2 = double.Parse(p2);
+            peso3 = double.Parse(p3);
+
             notaponderada1 = n1 * (peso1 / 100);
             notaponderada2 = n2 * (peso2 / 100);
             notaponderada3 = n3 * (peso3 / 100);
+
+
 
             promediofinal = notaponderada1 + notaponderada2 + notaponderada3;
 
